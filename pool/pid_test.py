311afbe69781622a1817d1666d717bf3e82a7943
opckaiyo2 <opckaiyo2@gmail.com>
2021-06-29 11:05:53 +0900

    pid fix

--- pool/pid_test.py
+++ pool/pid_test.py
@@ -7,68 +7,74 @@ import sys
 sys.path.append("/home/pi/2021/pool/my_mod")
 from motor_controller import Motor
 
-def fix_yaw(yaw2goal, yaw_now, max_correct, correct_yaw):
-  motor = Motor()
+class PID_FIX:
+  def __init__(self):
+    self.e = 0
+    self.e1 = 0
+    self.e2 = 0
+    self.M = 0
+    self.M1 = 0
 
-  M = 0.00
-  M1 = 0.00
-  e = 0.00
-  e1 = 0.00
-  e2 = 0.00
-  #Kp = 0.1
-  #Ki = 0.01
-  #Kd = 0.001
-  Kp = 0.001
-  Ki = 0.01
-  Kd = 0.5
+  def fix_yaw(self,yaw2goal, yaw_now, max_correct, correct_yaw):
+    motor = Motor()
 
-
-  max_correctVal = max_correct
-  if abs(yaw2goal - yaw_now) > 180:
-    e = 360 - abs(yaw2goal - yaw_now)
-  else:
-    e = abs(yaw2goal - yaw_now)
-
-  M = M1 + Kp * (e-e1) + Ki * e + Kd * ((e-e1) - (e1-e2))
-  direction = turn_dir(yaw_now, yaw2goal)
-
-  if M > max_correctVal:
-    M = max_correctVal
-  elif M < 0:
-    M = 0
-
-  correct_yaw = (M * direction)
-
-  M1 = M
-  e1 = e
-  e2 = e1
-  #time.sleep(1.0)
-
-  print("目標 : " + str(yaw2goal))
-  print("現在 : " + str(yaw_now))
-  print("操作量 : " + str(M))
-  print("方向+操作量 : " + str(correct_yaw))
-  print("")
-
-  motor.turn(M)
+    """
+    Kp = 0.1
+    Ki = 0.01
+    Kd = 0.001
+    #"""
+    Kp = 0.001
+    Ki = 0.01
+    Kd = 0.5
+    #"""
 
 
-def turn_dir(yaw_now, goal2yaw):
-  direction = 0
-
-  if yaw_now <= 180:
-    if -180 < yaw_now - goal2yaw < 0:
-      direction = -1
+    max_correctVal = max_correct
+    if abs(yaw2goal - yaw_now) > 180:
+      self.e = 360 - abs(yaw2goal - yaw_now)
     else:
-      direction = 1
+      self.e = abs(yaw2goal - yaw_now)
 
-  elif yaw_now <= 360:
-    if 0 < yaw_now - goal2yaw < 180:
-      direction = 1
-    else:
-      direction = -1
-    
-  return direction 
+    self.M = self.M1 + Kp * (self.e-self.e1) + Ki * self.e + Kd * ((self.e-self.e1) - (self.e1-self.e2))
+    direction = self.turn_dir(yaw_now, yaw2goal)
+
+    if self.M > max_correctVal:
+      self.M = max_correctVal
+    elif self.M < 0:
+      self.M = 0
+
+    correct_yaw = (self.M * direction)
+
+    self.e2 = self.e1
+    self.e1 = self.e
+    self.M1 = self.M
+    #time.sleep(1.0)
+
+    print("目標 : " + str(yaw2goal))
+    print("現在 : " + str(yaw_now))
+    print("操作量 : " + str(self.M))
+    print("方向+操作量 : " + str(correct_yaw))
+    print("")
+
+    motor.turn(self.M)
+
+
+  def turn_dir(self,yaw_now, goal2yaw):
+    direction = 0
+
+    if yaw_now <= 180:
+      if -180 < yaw_now - goal2yaw < 0:
+        direction = -1
+      else:
+        direction = 1
+
+    elif yaw_now <= 360:
+      if 0 < yaw_now - goal2yaw < 180:
+        direction = 1
+      else:
+        direction = -1
+      
+    return direction 
 
 
 def fix_depth(depth2goal, depth_now, correct_depth):
@@ -115,6 +121,8 @@ if __name__ == '__main__':
   max_correct = 30
   correct_yaw = 0
 
+  pid = PID_FIX()
+
   while(True):
     ser = serial.Serial('/dev/ttyACM0', 9600)
 
@@ -136,12 +144,13 @@ if __name__ == '__main__':
 
     try:
 
-      fix_yaw(yam2goal,yaw_now,max_correct,correct_yaw)
+      pid.fix_yaw(yam2goal,yaw_now,max_correct,correct_yaw)
     
     except KeyboardInterrupt:
       motor.stop()
       break
 
-    except:
+    except Exception as e:
       motor.stop()
+      print(e)
       break
