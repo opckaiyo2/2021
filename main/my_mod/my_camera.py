e72e081505a5e292cc8f1e78507f0eac6b8951da
opckaiyo2 <opckaiyo2@gmail.com>
2021-07-02 12:11:07 +0900

    20210702 final

--- main/my_mod/my_camera.py
+++ main/my_mod/my_camera.py
@@ -1,5 +1,5 @@
 import cv2
-import numpy
+import numpy as np
 
 def cap_main(hoop_Coordinate):
     try:
@@ -12,22 +12,25 @@ def cap_main(hoop_Coordinate):
                 raise ValueError("Failed to obtain camera image.")
 
             gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
-            circles = cv2.HoughCircles(gray,cv2.HOUGH_GRADIENT,1,20,param1=100,param2=60,minRadius=0,maxRadius=0)
+            kernel = np.ones((5,5),np.float32)/25
+            dst = cv2.filter2D(gray,-1,kernel)
+            circles = cv2.HoughCircles(dst,cv2.HOUGH_GRADIENT,1,20,param1=75,param2=60,minRadius=0,maxRadius=0)
             hoop_Coordinate = circles
 
             #"""
             # 検出した円を描画するpg
             # circles = cv2.HoughCirclesで円が検出できないとnumpyarrayにならないからerror
-            if(circles.size != 0):
+            if(circles is not None):
                 for i in circles[0,:]:
                     # draw the outer circle
                     cv2.circle(gray,(i[0],i[1]),i[2],(0,255,0),2)
                 
-                cv2.imshow("frame", gray)
-                cv2.waitKey(1)
+            cv2.imshow("frame", gray)
+            cv2.waitKey(1)
             #"""
 
     except Exception as e:
+        print("circles typw",type(circles))
         print("\n")
         print("my_camera.py cap_main try error : ",e)
         print("\n")
